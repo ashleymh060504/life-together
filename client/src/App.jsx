0dcd8dc40df45d655100c71cf9711de57075f8ed
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-02 22:17:14 -0500

    worked on main.jsx router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import InviteFriendsModal from './components/contact-us.jsx'
 import Homehtml from './html.jsx'
 
