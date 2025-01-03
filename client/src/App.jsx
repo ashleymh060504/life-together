fcbee8ddade6ef47febb387c3b6feb379f6de8d8
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-02 20:52:35 -0500

    removed link to app.css in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import InviteFriendsModal from './components/contact-us.jsx'
 import Homehtml from './html.jsx'
 
