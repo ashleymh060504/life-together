03a8c5432b91df5b82aecb96c1f5f97399181fbf
Mathew Lopez <mathewlopez10@gmail.com>
2025-01-06 18:38:10 -0500

    Completed Outdoors routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { createRoot, ReactDOM } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot, ReactDOM } from 'react-dom/client';
+import './index.css';
 
 import App from './App.jsx'
 import Together from './pages/Together.jsx'
