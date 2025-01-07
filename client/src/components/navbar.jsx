944d0748037d8643f2738c6d2faf3c75b773f87f
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-06 22:09:30 -0500

    tweaked navbar and added to together and user pgs

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -9,13 +9,13 @@ function Navbar() {
             <div class="collapse navbar-collapse" id="navbarText">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link active" aria-current="page" href="/">Logout</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Together</a>
+                  <a class="nav-link" href="/together">Together</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">User</a>
+                  <a class="nav-link" href="/user">User</a>
                 </li>
               </ul>
               <span class="navbar-text">
