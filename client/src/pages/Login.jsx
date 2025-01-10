0e344b4cf6bdfa4afae40b703f7023950f1b8d0a
Mathew Lopez <mathewlopez10@gmail.com>
2025-01-09 20:54:55 -0500

    Changed case for 'LoginRegister.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import Welcome from '../components/login-page/Welcome.jsx';
 
-import LoginRegister from '../components/login-page/loginRegister.jsx';
+import LoginRegister from '../components/login-page/LoginRegister.jsx';
 import ContactUs from '../components/login-page/ContactUs.jsx';
 
 function Login () {
