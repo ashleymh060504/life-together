d98d600f22f411983b2c0ac4343c4500f810592f
Ashley Hayes <ashleymh060504@gmail.com>
2025-01-16 13:31:45 -0500

    edited pkgjson and server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
 
 sequelize.sync()
     .then(() => {
